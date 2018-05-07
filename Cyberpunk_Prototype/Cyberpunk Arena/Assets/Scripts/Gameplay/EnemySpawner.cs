e7b5519c68af5c7d3036801ceddfd4730ac3eb55
LKZsaldanha <38111193+LKZsaldanha@users.noreply.github.com>
2018-05-07 18:47:53 -0300

    Spawn de inimigos

---
+++ Cyberpunk_Prototype/Cyberpunk Arena/Assets/Scripts/Gameplay/EnemySpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+	public PlayerHealth playerHealth;
+	public GameObject enemy;
+	public float spawnTime;
+	public Transform[] spawnPoints;
+
+
+	// Use this for initializations
+	void Start () {
+		InvokeRepeating("Spawn", spawnTime, spawnTime);
+	}
+
+	void Spawn(){
+		if(playerHealth.currentHealth<=0){	
+			return;
+		}
+
+		int spawnPointIndex = Random.Range(0,spawnPoints.Length);
+
+		Instantiate (enemy, spawnPoints[spawnPointIndex].position,spawnPoints[spawnPointIndex].rotation);
+	}
+}
