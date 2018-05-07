906f413392eebf6b7aa9cf9d80edf113f9e34906
LKZsaldanha <38111193+LKZsaldanha@users.noreply.github.com>
2018-05-07 14:57:04 -0300

    Scripts de movimentação
    movimentação do player e do inimigo (segue jogador) (precisa de navmesh e da tag "player")

---
+++ Cyberpunk_Prototype/Cyberpunk Arena/Assets/Scripts/Gameplay/EnemyMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(Rigidbody))]
+public class EnemyMovement : MonoBehaviour {
+	
+	Rigidbody enemyRigidbody;
+
+	Transform player;
+	NavMeshAgent nav;
+
+	void Awake(){
+		enemyRigidbody = GetComponent<Rigidbody>();
+
+		//Autoconfigure rigidbody
+		enemyRigidbody.drag = Mathf.Infinity;
+		enemyRigidbody.angularDrag = Mathf.Infinity;
+		enemyRigidbody.constraints = RigidbodyConstraints.FreezePositionY | 
+			RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+		nav = GetComponent<NavMeshAgent>();
+	}
+
+	void Update (){
+		nav.SetDestination(player.position);
+	}
+
+}
---
+++ Cyberpunk_Prototype/Cyberpunk Arena/Assets/Scripts/Gameplay/PlayerMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed = 5f;
+	public float rorationSmoothing = 10f;
+
+	Vector3 movement;
+	Rigidbody playerRigidbody;
+
+	void Awake(){
+		playerRigidbody = GetComponent<Rigidbody>();
+
+		//Autoconfigure rigidbody
+		playerRigidbody.drag = Mathf.Infinity;
+		playerRigidbody.angularDrag = Mathf.Infinity;
+		playerRigidbody.constraints = RigidbodyConstraints.FreezePositionY | 
+			RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+
+	}
+
+	void FixedUpdate(){
+		float h = Input.GetAxisRaw("Horizontal");
+		float v = Input.GetAxisRaw("Vertical");
+
+		Move(h,v);
+
+		Turning();
+
+	}
+
+	void Move(float h, float v){
+		movement.Set(h,0f, v);
+		movement = movement.normalized * speed * Time.deltaTime;
+
+		playerRigidbody.MovePosition (transform.position + movement);
+	}
+
+	void Turning(){
+		Vector3 lookDirection = movement*Time.deltaTime;;
+		lookDirection.y = 0f;
+
+		Quaternion newRotation = Quaternion.LookRotation(lookDirection);
+		Quaternion finalRotation = Quaternion.Slerp(transform.rotation, newRotation, Time.deltaTime*rorationSmoothing);
+		playerRigidbody.MoveRotation(finalRotation);
+	
+	}
+
+}
