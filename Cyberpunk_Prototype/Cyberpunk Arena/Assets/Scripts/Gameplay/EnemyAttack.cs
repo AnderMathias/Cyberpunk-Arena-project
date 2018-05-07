c7390e48e038ae6c3b2cf1cd21ad6bf79af5a63e
LKZsaldanha <38111193+LKZsaldanha@users.noreply.github.com>
2018-05-07 17:12:25 -0300

    Ajustes no prefab do player

---
+++ Cyberpunk_Prototype/Cyberpunk Arena/Assets/Scripts/Gameplay/EnemyAttack.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour {
+
+	public int timeBetweenAttacks= 0.5f;
+	public int attackDamage = 10;
+
+	GameObject player;
+	bool playerInRange;
+	float timer;
+
+
+	void Awake () {
+		player = GameObject.FindGameObjectWithTag("Player");		
+	}
+
+	void OnTriggerEnter (Collider other){
+		if(other.gameObject == player){
+			playerInRange = true;
+		}
+	}
+
+}
