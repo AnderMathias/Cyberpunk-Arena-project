0c23840e114855c6bfeceed226c4071770ded4cc
LKZsaldanha <38111193+LKZsaldanha@users.noreply.github.com>
2018-05-09 22:15:50 -0300

    Inimigo some depois de morto

--- Cyberpunk_Prototype/Cyberpunk Arena/Assets/Scripts/Gameplay/Enemy/EnemyHealth.cs
+++ Cyberpunk_Prototype/Cyberpunk Arena/Assets/Scripts/Gameplay/Enemy/EnemyHealth.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyHealth : MonoBehaviour {
 
 	public int startingHealth = 100;
 	public int currentHealth;
+	public float sinkSpeed = 0.5f;
+	public float sinkDelay = 2f;
 
-
+	Animator anim;
 	CapsuleCollider capsuleCollider;
 	bool isDead;
-	Animator anim;
+	bool isSinking;
+
 
 	void Awake() {
 		capsuleCollider = GetComponent <CapsuleCollider>();
@@ -21,7 +25,9 @@ public class EnemyHealth : MonoBehaviour {
 	
 
 	void Update () {
-		
+		if(isSinking){
+			transform.Translate(-Vector3.up * sinkSpeed * Time.deltaTime);
+		}
 	}
 
 	public void TakeDamage(int amount){
@@ -40,7 +46,16 @@ public class EnemyHealth : MonoBehaviour {
 	void Death(){
 		isDead = true;
 
+		Invoke("StartSinking", sinkDelay);
 		capsuleCollider.isTrigger = true;
 		anim.SetTrigger("Dead");
+	}
+
+	void StartSinking(){
+		GetComponent<NavMeshAgent>().enabled = false;
+		GetComponent<Rigidbody>().isKinematic = true;
+		isSinking = true;
+
+		Destroy (gameObject,3f);
 	}
 }
